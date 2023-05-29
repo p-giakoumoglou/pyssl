8edd31b6eca9e61c33b4833c8e74fcc4145fa716
Nikolaos Giakoumoglou <nikolaos.giakoumoglou@gmail.com>
2023-05-29 12:15:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def set_optimizer(optimizer_name, model, lr, momentum, weight_decay):
 def set_lr_scheduler(optimizer, warmup_epochs, warmup_lr, num_epochs, base_lr, final_lr, iter_per_epoch, constant_predictor_lr=False):
     """ Learning Rate Scheduler for Self-Supervised Learning """
     scheduler = LearningRateScheduler(optimizer, warmup_epochs, warmup_lr, num_epochs, base_lr, final_lr, iter_per_epoch, True)
-    print(f'Cosine learning rate decay scheduler initialized (warmup_epochs={warmup_epochs}, warmup_lr={warmup_lr}, base_lr={final_lr}, final_lr={final_lr})')
+    print(f'Cosine learning rate decay scheduler initialized (warmup_epochs={warmup_epochs}, warmup_lr={warmup_lr}, base_lr={base_lr}, final_lr={final_lr})')
     return scheduler
 
 
