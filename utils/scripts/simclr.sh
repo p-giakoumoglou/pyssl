bbaee2984595d656018c8f6cd091792a781209d6
Nikolaos Giakoumoglou <nikolaos.giakoumoglou@gmail.com>
2023-05-29 17:35:14 +0300

    Add files via upload

--- utils/scripts/simclr.sh
+++ utils/scripts/simclr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python main.py --model_name=simclr --backbone=resnet50 --batch_size=512 --optimizer=lars_simclr --weight_decay=0.000001 --momentum=0.9 --stop_at_epoch=100 --warmup_epochs=10 --warmup_lr=0 --base_lr=0.5 --final_lr=0.5 --num_epochs=100
+python main.py --model_name=simclr --backbone=resnet18 -batch_size=512 --optimizer=lars_simclr --weight_decay=0.0001 --momentum=0.9 --warmup_epochs=10 --warmup_lr=0 --base_lr=1.0 --final_lr=0 --num_epochs=1000
 
-python main_linear.py --model_name=simclr --backbone=resnet50 --batch_size=512 --optimizer=sgd_nesterov --weight_decay=0 --momentum=0.9 --warmup_epochs=0 --warmup_lr=0 --base_lr=0.1 --final_lr=0.1 --num_epochs=300
+python main_linear.py --model_name=simclr --backbone=resnet18--batch_size=512 --optimizer=sgd_nesterov --weight_decay=0 --momentum=0.9 --warmup_epochs=0 --base_lr=0.1 --final_lr=0 --num_epochs=100
--- utils/scripts/simsiam.sh
+++ utils/scripts/simsiam.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python main.py --model_name=simsiam --backbone=resnet18 --batch_size=512 --optimizer=sgd --weight_decay=0.0005 --momentum=0.9 --stop_at_epoch=800 --warmup_epochs=10 --warmup_lr=0 --base_lr=0.03 --final_lr=0 --num_epochs=800
+python main.py --model_name=simsiam --backbone=resnet18 --batch_size=512 --optimizer=sgd --weight_decay=0.0005 --momentum=0.9 --warmup_epochs=10 --warmup_lr=0 --base_lr=0.03 --final_lr=0 --num_epochs=800
 
-python main_linear.py --model_name=simsiam --backbone=resnet18 --batch_size=256 --optimizer=sgd --weight_decay=0 --momentum=0.9 --warmup_epochs=10 --warmup_lr=0 --base_lr=30 --final_lr=0 --num_epochs=100
+python main_linear.py --model_name=simsiam --backbone=resnet18 --batch_size=256 --optimizer=sgd --weight_decay=0 --momentum=0.9 --warmup_epochs=0 --base_lr=30 --final_lr=0 --num_epochs=100
+
+python main_linear.py --model_name=simsiam --backbone=resnet18 --batch_size=256 --optimizer=lars --weight_decay=0 --momentum=0.9 --warmup_epochs=0 --base_lr=0.02 --final_lr=0 --num_epochs=100
