fab4b564a45f0b7f3579da2ef0cb175f3890b8b5
Nikolaos Giakoumoglou <nikolaos.giakoumoglou@gmail.com>
2023-06-01 23:14:56 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,11 +3,10 @@ from .average_meter import AverageMeter
 from .early_stopping import EarlyStopping
 from .ema import EMA
 from .seeds import set_deterministic, set_all_seeds
-from .timestamp import timestamp
 from .cosine_schedulers import CosineDecayLR, CosineDecayWD
 
-__all__ = ['calculate_topk_accuracy', 'AverageMeter', 'colorstr', 'emojis', 
+__all__ = ['calculate_topk_accuracy', 'AverageMeter',
            'EarlyStopping', 'EMA', 'CosineDecayLR', 'CosineDecayWD',
-           'set_deterministic', 'set_all_seeds', 'timestamp',
+           'set_deterministic', 'set_all_seeds', 
            ]
 
