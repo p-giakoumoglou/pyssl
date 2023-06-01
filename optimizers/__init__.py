963d9f7efe2a712f744ba691063183d59230cefa
Nikolaos Giakoumoglou <nikolaos.giakoumoglou@gmail.com>
2023-06-01 23:13:09 +0300

    Update __init__.py

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,6 +1,5 @@
 from .larc import LARC
 from .lars import LARS
 from .lars_simclr import LARS_SimCLR
-from .cosine_decay_warmup import CosineDecayWarmup
 
-__all__ = ['LARC', 'LARS', 'LARS_SimCLR', 'CosineDecayWarmup']
+__all__ = ['LARC', 'LARS', 'LARS_SimCLR']
