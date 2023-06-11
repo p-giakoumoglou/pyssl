84223ed52da4d835812cdbf88439a6d38af0458c
Nikolaos Giakoumoglou <nikolaos.giakoumoglou@gmail.com>
2023-06-11 20:50:12 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .byol import BYOL
-from .simclr import SimCLR
-from .simsiam import SimSiam
-from .supcon import SupCon
-from .swav import SWAV
-from .dino import DINO
-
-__all__ = ['SimCLR', 'SimSiam', 'SupCon', 'BYOL']
